97b8244dbf23480c8599160128804376a7cb2a9d
Moises Baez <supercapri71@gmail.com>
2025-09-23 18:58:01 -0500

    ejercicio saluda

--- ejer_git1.js
+++ ejer_git1.js
@@ -1,10 +1,18 @@
-obtieneMinuscula=function(){
+obtieneMinuscula = function () {
       let nota1;
-     let nota2;
-     let cmpResu;
-     cmpResu = document.getElementById("txtNota1");
-     nota1 = cmpResu.value;
-     nota2=parseInt(nota1.charCodeAt(0));
-     prome = valeMinu(nota2);
-     mostrarTexto("lblProme", prome);
+      let nota2;
+      let cmpResu;
+      cmpResu = document.getElementById("txtNota1");
+      nota1 = cmpResu.value;
+      nota2 = parseInt(nota1.charCodeAt(0));
+      prome = valeMinu(nota2);
+      mostrarTexto("lblProme", prome);
 }
+saludo = function () {
+      let prome;
+      let nombre = recuperarTexto("txtNombre");
+      let apellido = recuperarTexto("txtApellido");
+      prome = nombre + " " + apellido;
+      mostrarTexto("lblProme", prome);
+}
+
